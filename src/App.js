cf07ef7e083881b3cbdcd61e570ce3f87c8e9ab4
Seimon <seimon@Seimons-iMac.local>
2016-11-29 14:17:52 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Message from './Message';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+
+        <p>sadwadsa says </p>  <Message /><Message /><Message /><Message />
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Message.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Message extends Component {
+  render() {
+    return (
+
+        <p>WASSSSSSSSSUUUUUUPPPPP </p> 
+
+    );
+  }
+}
+
+export default Message;
